b58b92ee29784fe1c8b8353da153fd6b6b472842
Prem Bahadur Chand <pcmsia@gmail.com>
2020-08-05 16:12:14 +0200

    I have created simple ebook reader using javascript and sass

--- js/index.js
+++ js/index.js
@@ -1,4 +1,6 @@
-//Load a book from disk
+//////////////////////////////////////////////////
+/////////////Load a book from disk///////////////
+///////////////////////////////////////////////
 
 function loadBook(filename, displayName) {
     let currentBook = "";
@@ -23,7 +25,7 @@ function loadBook(filename, displayName) {
 
             currentBook = xhr.responseText;
 
-           // getDocumentStatistics(currentBook);
+            getDocStatus(currentBook);
            
 
             //remove line breaks and carriage returns and replace with a <br>
@@ -31,31 +33,37 @@ function loadBook(filename, displayName) {
 
             document.querySelector("#fileContent").innerHTML = currentBook;
 
-            const element = document.querySelector("#fileContent");
-            element.scrollTop = 0; //Scroll viewer to top
+            let elmnt = document.querySelector("#fileContent");
+            elmnt.scrollTop = 0; //Scroll viewer to top
 
         }
     };
 }
+///////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////
+//////////////get the stats for the book/////////////////
+//////////////////////////////////////////////////////////
 
-//get the stats for the book
-/* function getDocumentStatistics(fileContent) {
+function getDocStatus(fileContent) {
 
-    const documentLength = document.querySelector("#documentLength");
+    const docLength = document.querySelector("#docLength");
     const wordCount = document.querySelector("#wordCount");
-    const characterCount = document.querySelector("#characterCount");
+    const charCount = document.querySelector("#charCount");
 
     let text = fileContent.toLowerCase();
-    let wordArray = text.match(/\b\S+\b/g);
-    let wordDictionary = {};
 
-    let uncommonWords = [];
+    let wordArray = text.match(/\b\S+\b/g); //It looks for spaces with a slash B's and then characters in between any two spaces it will return the array of words
+    let wordDictionary = {}; //
 
-    //filter out the uncommon words
+    let uncommonWords = [];  // This is for un common word that we do not want to count.
+
+    /////filter out the uncommon words//////////////
+
     uncommonWords = filterStopWords(wordArray);
 
-
-    //Count every word in the wordArray
+    ////////////////////////////////////////////////////////////
+    ///////////Count every word in the wordArray////////////////
+    ///////////////////////////////////////////////////////////
 
     for (let word in uncommonWords) {
         let wordValue = uncommonWords[word];
@@ -66,25 +74,25 @@ function loadBook(filename, displayName) {
         }
     }
 
-
-    //sort the array
+     /////////////sort the array//////////////////
     let wordList = sortProperties(wordDictionary);
+    ///////////Return the top 5 words/////////////
+    let top5Words = wordList.slice(0, 6);
+    //////return the least 5 words////////////////////////
+    let least5Words = wordList.slice(-6, wordList.length);
 
-    //Return the top 5 words
-    let topFiveWords = wordList.slice(0, 6);
-    //return the least 5 words
-    let leastFiveWords = wordList.slice(-6, wordList.length);
+    /////////Write the values to the page//////////////
+    ULTemplate(top5Words, document.querySelector("#mostUsed"));
+    ULTemplate(least5Words, document.querySelector("#leastUsed"));
 
-    //Write the values to the page
-    UserTemplate(topFiveWords, document.querySelector("#mostUsed"));
-    UserTemplate(leastFiveWords, document.querySelector("#leastUsed"));
-
-    documentLength.innerText = "Document Length: " + text.length;
-    wordCount.innerText = "Word Count: " + wordArray.length;
+    docLength.innerText = "Document Length: " + text.length; // it will shows the doucment length on Document Staticis section
+    wordCount.innerText = "Word Count: " + wordArray.length;// it will count the doucment length on Document Staticis section
 
 }
 
-function UserTemplate(items, element) {
+
+function ULTemplate(items, element) {
+
     let rowTemplate = document.querySelector('#template-ul-items');
     let templateHTML = rowTemplate.innerHTML;
     let resultsHTML = "";
@@ -101,18 +109,21 @@ function sortProperties(obj) {
 
     //first convert the object to an array
 
-    let returnArray = Object.entries(obj);
+    let rtnArray = Object.entries(obj);
 
     //Sort the array
-    returnArray.sort((first, second) => {
+    rtnArray.sort((first, second) => {
         return second[1] - first[1];
     });
 
-    return returnArray;
+    return rtnArray;
 
 }
 
-//filter out stop words
+///////////////////////////////////
+/////filter out stop words//////////
+////////////////////////////////////
+
 function filterStopWords(wordArray) {
     const commonWords = getStopWords();
     const commonObj = {};
@@ -132,12 +143,18 @@ function filterStopWords(wordArray) {
     return uncommonArr;
 }
 
-//a list of stop words we don't want to include in stats
+////////////////////////////////////////////////////////////
+//a list of stop words we don't want to include in stats////
+///////////////////////////////////////////////////////////
+
 function getStopWords() {
-    return ["a", "able", "about", "across", "after", "all", "almost", "also", "am", "among", "an", "and", "any", "are", "as", "at", "be", "because", "been", "but", "by", "can", "cannot", "could", "dear", "did", "do", "does", "either", "else", "ever", "every", "for", "from", "get", "got", "had", "has", "have", "he", "her", "hers", "him", "his", "how", "however", "i", "if", "in", "into", "is", "it", "its", "just", "least", "let", "like", "likely", "may", "me", "might", "most", "must", "my", "neither", "no", "nor", "not", "of", "off", "often", "on", "only", "or", "other", "our", "own", "rather", "said", "say", "says", "she", "should", "since", "so", "some", "than", "that", "the", "their", "them", "then", "there", "these", "they", "this", "tis", "to", "too", "twas", "us", "wants", "was", "we", "were", "what", "when", "where", "which", "while", "who", "whom", "why", "will", "with", "would", "yet", "you", "your", "ain't", "aren't", "can't", "could've", "couldn't", "didn't", "doesn't", "don't", "hasn't", "he'd", "he'll", "he's", "how'd", "how'll", "how's", "i'd", "i'll", "i'm", "i've", "isn't", "it's", "might've", "mightn't", "must've", "mustn't", "shan't", "she'd", "she'll", "she's", "should've", "shouldn't", "that'll", "that's", "there's", "they'd", "they'll", "they're", "they've", "wasn't", "we'd", "we'll", "we're", "weren't", "what'd", "what's", "when'd", "when'll", "when's", "where'd", "where'll", "where's", "who'd", "who'll", "who's", "why'd", "why'll", "why's", "won't", "would've", "wouldn't", "you'd", "you'll", "you're", "you've"];
+    return ["a", "able", "about", "across", "after", "all", "almost", "also", "am", "among", "an", "and", "any", "are", "as", "at", "be", "because", "been", "but", "by", "can", "cannot", "could", "dear", "did", "do", "does", "either", "else", "ever", "every", "for", "from", "get", "got", "had", "has", "have", "he", "her", "hers", "him", "his", "how", "however", "i", "if", "in", "into", "is", "it", "its", "just", "least", "let", "like", "likely", "may", "me", "might", "most" , "mr", "must", "my", "neither", "no", "nor", "not", "of", "off", "often", "on", "only", "or", "other", "our", "own", "rather", "said", "say", "says", "she", "should", "since", "so", "some", "than", "that", "the", "their", "them", "then", "there", "these", "they", "this", "tis", "to", "too", "twas", "us", "wants", "was", "we", "were", "what", "when", "where", "which", "while", "who", "whom", "why", "will", "with", "would", "yet", "you", "your", "ain't", "aren't", "can't", "could've", "couldn't", "didn't", "doesn't", "don't", "hasn't", "he'd", "he'll", "he's", "how'd", "how'll", "how's", "i'd", "i'll", "i'm", "i've", "isn't", "it's", "might've", "mightn't", "must've", "mustn't", "shan't", "she'd", "she'll", "she's", "should've", "shouldn't", "that'll", "that's", "there's", "they'd", "they'll", "they're", "they've", "wasn't", "we'd", "we'll", "we're", "weren't", "what'd", "what's", "when'd", "when'll", "when's", "where'd", "where'll", "where's", "who'd", "who'll", "who's", "why'd", "why'll", "why's", "won't", "would've", "wouldn't", "you'd", "you'll", "you're", "you've"];
 }
 
-//highlight the words in search
+//////////////////////////////////////////////////////
+/////////////highlight the words in search////////////
+////////////////////////////////////////////////////
+
 function highlightTheWordsInSearch() {
 
     //read the keyword
@@ -148,21 +165,20 @@ function highlightTheWordsInSearch() {
     let newContent = "";
 
     //find all the currently marked items
-    let spans = document.querySelectorAll('mark');
-
-    //<mark>Harry</mark>
-    //Harry
+    let spans = document.querySelectorAll('mark'); // it will look for the mark, like this <mark>Harry</mark>
+    
 
     for (let i = 0; i < spans.length; i++) {
-        spans[i].outerHTML = spans[i].innerHTML;
+        spans[i].outerHTML = spans[i].innerHTML; // outerHTML will be this: <mark>Harry</mark> innerHTML will be just "Harry"
+        //what it does is it takes string here this <mark>Harry</mark> and takes "Harry" from there.
     }
 
-    let regularExpress = new RegExp(keyword, "gi");
+    let re = new RegExp(keyword, "gi"); //look for globally (g) and i don't care about case sensetive (i);
     let replaceText = "<mark id ='markme'>$&</mark>";
     let bookContent = display.innerHTML;
 
     //add the mark to the book content
-    newContent = bookContent.replace(regularExpress, replaceText);
+    newContent = bookContent.replace(re,replaceText);
 
     display.innerHTML = newContent;
     let count = document.querySelectorAll('mark').length;
@@ -173,4 +189,5 @@ function highlightTheWordsInSearch() {
         element.scrollIntoView();
     };
 
-} */
+}
+
